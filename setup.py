04aacd7bb5ef5eb597db0e68435bc23ea2cb4527
Martin Höss <info@martin-hoess.de>
2023-02-17 14:14:26 +0100

    Support for HmIP-PSM-2, bump version to 0.1.99

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 PACKAGE_NAME = 'pyhomematic'
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = '0.1.88'
+VERSION = '0.1.99'
 
 PACKAGES = find_packages(exclude=['dist', 'build', 'tests'])
 
